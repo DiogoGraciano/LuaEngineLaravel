9067d2e9676c3b1c1e4a318051a81deed1c6fc32
Diogo Graciano <diogo.dg691@gmail.com>
2025-10-30 17:47:09 -0300

    updated facade with methods

--- src/Facades/LuaEngine.php
+++ src/Facades/LuaEngine.php
@@ -8,12 +8,23 @@ use DiogoGraciano\LuaEngine\Contracts\LuaEngineInterface;
 /**
  * Facade for Lua Engine
  * 
- * @method static mixed execute(string $script, array $data = [])
- * @method static bool evaluate(string $script, array $data = [])
+ * @method static mixed execute(string $script)
+ * @method static bool evaluate(string $script)
  * @method static bool validate(string $script)
- * @method static void registerFunction(string $name, callable $callback)
+ * @method static void registerLibrary(array $functions, string $libName = 'php')
+ * @method static void registerFunction(string $functionName, callable $function, string $libName = 'php')
+ * @method static array getRegisteredLibrary()
+ * @method static array getRegisteredFunctions(string $libName = 'php')
+ * @method static bool isRegisteredFunction(string $libName = 'php', string $functionName)
+ * @method static \LuaSandboxFunction|false loadString(string $code)
+ * @method static array|false callFunction(string $name, mixed ...$args)
+ * @method static \LuaSandboxFunction|false wrapPhpFunction(callable $function)
+ * @method static void setMemoryLimit(int $bytes)
+ * @method static void setCPULimit(float $seconds)
+ * @method static ?\LuaSandbox getSandbox()
  * @method static string|null getLastError()
  * @method static bool isAvailable()
+ * @method static mixed executeFile(string $filePath, array $data = [])
  * 
  * @see \DiogoGraciano\LuaEngine\LuaEngine
  */
